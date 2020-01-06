55e16db29003a9ea3e88932de395c885cc1d8d0e
Mauricio-KND <1299@holbertonschool.com>
2020-01-06 18:31:40 +0000

    Fix of swap function

--- op_swap.c
+++ op_swap.c
@@ -8,6 +8,7 @@
 void op_swap(stack_t **stack, unsigned int line_number)
 {
 	stack_t *new = *stack;
+	int i;
 	(void) line_number;
 
 	if (!new || !new->next)
@@ -16,14 +17,10 @@ void op_swap(stack_t **stack, unsigned int line_number)
 		free_files(stack);
 		exit(EXIT_FAILURE);
 	}
-
-	*stack = new->next;
 	if ((*stack)->next != NULL)
 	{
-		(*stack)->next->prev = new;
-		new->next = (*stack)->next;
-		new->prev = *stack;
-		(*stack)->next = new;
-		(*stack)->prev = NULL;
+		i = (*stack)->n;
+		(*stack)->n = (*stack)->next->n;
+		(*stack)->next->n = i;
 	}
 }
