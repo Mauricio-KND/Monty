9d0d70f24f47b9c661163837d1f82e0c68e063b4
Mauricio-KND <1299@holbertonschool.com>
2020-01-06 04:05:57 +0000

    Some corrections

--- op_pint.c
+++ op_pint.c
@@ -12,7 +12,7 @@ void op_pint(stack_t **stack, unsigned int line_number)
 
 	if (!new)
 	{
-		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 		free_files(stack);
 		exit(EXIT_FAILURE);
 	}
